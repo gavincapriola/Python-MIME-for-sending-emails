f1ccc287fdc0dd78275b6ff3b0a07b2509251733
Gavin Capriola <gavincapriola@gmail.com>
2022-08-27 15:53:51 -0400

    define send_email function

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+
+def send_email(fromaddr, to_addrs, subject, body, attachment, password):
+    pass  
+
+
+if __name__ == '__main__':
+    fromaddr = ''
+    to_addrs = []
+    subject = ''
+    body = ''
+    attachment = ''
+    password = ''
+    
+    send_email(fromaddr, to_addrs, subject, body, attachment, password)
